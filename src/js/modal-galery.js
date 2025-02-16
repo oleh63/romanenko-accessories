b19986c9bd76c967d9feb412325e5a3ae321e9c2
Oleh Skrypets <olehskrupets@gmail.com>
2025-02-16 19:52:32 +0200

    add import img

--- src/js/modal-galery.js
+++ src/js/modal-galery.js
@@ -1,6 +1,58 @@
 // modal-catalog.js
 import branchletcard from '../img/branchletcard.webp';
-// Отримуємо елементи DOM
+
+import earringscard from '../img/earringscard.webp';
+import earrings1 from '../img/earrings1.webp';
+import earrings2 from '../img/earrings2.webp';
+import earrings3 from '../img/earrings3.webp';
+import earrings4 from '../img/earrings4.webp';
+import earrings5 from '../img/earrings5.webp';
+import earrings6 from '../img/earrings6.webp';
+import earrings7 from '../img/earrings7.webp';
+import earrings8 from '../img/earrings8.webp';
+import earrings9 from '../img/earrings9.webp';
+import earrings10 from '../img/earrings10.webp';
+
+import hoopscard from '../img/hoopscard.webp';
+import hoops1 from '../img/hoops1.webp';
+import hoops2 from '../img/hoops2.webp';
+import hoops3 from '../img/hoops3.webp';
+import hoops4 from '../img/hoops4.webp';
+import hoops5 from '../img/hoops5.webp';
+import hoops6 from '../img/hoops6.webp';
+import hoops7 from '../img/hoops7.webp';
+import hoops8 from '../img/hoops8.webp';
+
+import necklacescard from '../img/necklacescard.webp';
+import necklaces1 from '../img/necklaces1.webp';
+import necklaces2 from '../img/necklaces2.webp';
+import necklaces3 from '../img/necklaces3.webp';
+import necklaces4 from '../img/necklaces4.webp';
+import necklaces5 from '../img/necklaces5.webp';
+import necklaces6 from '../img/necklaces6.webp';
+import necklaces7 from '../img/necklaces7.webp';
+import necklaces8 from '../img/necklaces8.webp';
+import necklaces9 from '../img/necklaces9.webp';
+import necklaces10 from '../img/necklaces10.webp';
+import necklaces11 from '../img/necklaces11.webp';
+import necklaces12 from '../img/necklaces12.webp';
+import necklaces13 from '../img/necklaces13.webp';
+import necklaces14 from '../img/necklaces14.webp';
+import necklaces15 from '../img/necklaces15.webp';
+import necklaces17 from '../img/necklaces17.webp';
+import necklaces16 from '../img/necklaces16.webp';
+
+import combscard from '../img/combscard.webp';
+
+import pinscard from '../img/pinscard.webp';
+import pins1 from '../img/pins1.webp';
+import pins2 from '../img/pins2.webp';
+import pins3 from '../img/pins3.webp';
+import pins4 from '../img/pins4.webp';
+import pins5 from '../img/pins5.webp';
+import pins6 from '../img/pins6.webp';
+import pins7 from '../img/pins7.webp';
+
 const catalogList = document.querySelector('.list-catalog');
 const modal = document.getElementById('product-modal');
 const modalTitle = document.getElementById('modal-title');
@@ -18,75 +70,67 @@ const productData = {
     title: 'Сережки',
     price: 'Price',
     images: [
-      'earrings1.webp',
-      'earrings2.webp',
-      'earrings3.webp',
-      'earrings4.webp',
-      'earrings5.webp',
-      'earrings6.webp',
-      'earrings7.webp',
-      'earrings8.webp',
-      'earrings9.webp',
-      'earrings10.webp',
-      'earringscard.webp',
+      earrings1,
+      earrings2,
+      earrings3,
+      earrings4,
+      earrings5,
+      earrings6,
+      earrings7,
+      earrings8,
+      earrings9,
+      earrings10,
+      earringscard,
     ],
   },
   hoops: {
     title: 'Обручі',
     price: 'Price',
     images: [
-      'hoopscard.webp',
-      'hoops1.webp',
-      'hoops2.webp',
-      'hoops3.webp',
-      'hoops4.webp',
-      'hoops5.webp',
-      'hoops6.webp',
-      'hoops7.webp',
-      'hoops8.webp',
+      hoopscard,
+      hoops1,
+      hoops2,
+      hoops3,
+      hoops4,
+      hoops5,
+      hoops6,
+      hoops7,
+      hoops8,
     ],
   },
   necklaces: {
     title: 'Намиста',
     price: 'Price',
     images: [
-      'necklacescard.webp',
-      'necklaces1.webp',
-      'necklaces2.webp',
-      'necklaces3.webp',
-      'necklaces4.webp',
-      'necklaces5.webp',
-      'necklaces6.webp',
-      'necklaces7.webp',
-      'necklaces8.webp',
-      'necklaces9.webp',
-      'necklaces10.webp',
-      'necklaces12.webp',
-      'necklaces13.webp',
-      'necklaces14.webp',
-      'necklaces15.webp',
-      'necklaces16.webp',
-      'necklaces17.webp',
+      necklacescard,
+      necklaces1,
+      necklaces2,
+      necklaces3,
+      necklaces4,
+      necklaces5,
+      necklaces6,
+      necklaces7,
+      necklaces8,
+      necklaces9,
+      necklaces10,
+      necklaces11,
+      necklaces12,
+      necklaces13,
+      necklaces14,
+      necklaces15,
+      necklaces16,
+      necklaces17,
     ],
   },
   combs: {
     title: 'Гребінці',
     price: 'Price',
-    images: ['combscard.webp'],
+    images: [combscard],
   },
   pins: {
     title: 'Шпильки',
     price: 'Price',
-    images: [
-      'pinscard.webp',
-      'pins1.webp',
-      'pins2.webp',
-      'pins3.webp',
-      'pins4.webp',
-      'pins5.webp',
-      'pins6.webp',
-      'pins7.webp',
-    ],
+    images: [pinscard, pins1, pins2, pins3, pins4, pins5, pins6, pins7],
   },
 };
 
